033a8adfd2c1d182befcea97d11031106cf9a6e9
Raven <raveniv@ya.ru>
2024-03-27 22:58:24 +0500

    Форматирование скрипта

--- Контакты/main.py
+++ Контакты/main.py
@@ -1,14 +1,12 @@
 from sqlite3 import *
 from tkinter import *
 from tkinter import filedialog as fd
-from tkinter.messagebox import showerror, showwarning, showinfo
-from tkinter import simpledialog
 from easygui import *
 
 #Декларация
 path = ''
 db = None
-myfiletypes =[("DB files", "*.db"),
+my_filetypes =[("DB files", "*.db"),
                    ("db3 files", "*.db3;*.htm"),("sqlite files","*.sqlite"),
                    ("sqlite files","*.sqlite3"),("All files", "*.*")]
 records = []
@@ -16,35 +14,35 @@ records = []
 #Главное окно
 window = Tk()
 
-def ReadDB(Data : Connection, recindex):  #Opening DB
+def Read_db(Data : Connection, recindex):  #Opening DB
     global records
-    lblpath.config(text = path)    
+    lbl_path.config(text = path)    
     cur = Data.cursor()
     cur.execute('Select * From Контакты;')
     records = cur.fetchall()
     records.sort()
     Data.commit()
     if len(records) == 0:
-        showwarning(message='Записи отсутствуют', title='Внимание')
+        msgbox(msg='Записи отсутствуют', title='Внимание')
         return       
     item = records[recindex]
-    enttitle.delete(0, END)
-    enttitle.insert(0,item[0])    
-    Phones =[word.strip() for word in item[1].split(';')]        
-    Phones_var.set(Phones)
-    Mails =[word.strip() for word in item[2].split(';')]
-    boxmails.config(height=len(Mails))
-    Mails_var.set(Mails)
+    ent_title.delete(0, END)
+    ent_title.insert(0,item[0])    
+    phones =[word.strip() for word in item[1].split(';')]        
+    phones_var.set(phones)
+    mails =[word.strip() for word in item[2].split(';')]
+    box_mails.config(height=len(mails))
+    mails_var.set(mails)
     entday.delete(0, END)
     entday.insert(0,item[3])
-    Addons = [word.strip() for word in item[4].split(';')]
-    if len(Addons) > len(Phones):
-        boxAddons.config(height=len(Addons))
-        boxphone.config(height=len(Addons))
+    addons = [word.strip() for word in item[4].split(';')]
+    if len(addons) > len(phones):
+        box_addons.config(height=len(addons))
+        box_phone.config(height=len(addons))
     else:
-        boxAddons.config(height=len(Phones))
-        boxphone.config(height=len(Phones))
-    Addons_var.set(Addons)
+        box_addons.config(height=len(phones))
+        box_phone.config(height=len(phones))
+    addons_var.set(addons)
 
 def Connect(dbpath): #Подключение базы данных
     conn = Connection(dbpath)
@@ -62,39 +60,39 @@ def Connect(dbpath): #Подключение базы данных
     return conn
 
 #Обработчики для меню
-def OpenDB():
+def Open_db():
     global db
     global path
-    path = fd.askopenfilename(filetypes=myfiletypes, title='Выбор файла', initialfile='*.db') 
+    path = fd.askopenfilename(filetypes=my_filetypes, title='Выбор файла', initialfile='*.db') 
     db = Connect(path)
-    ReadDB(db, 0)  
+    Read_db(db, 0)  
     
  
-def NewDB():
+def New_db():
     global path
     global db    
-    path = fd.asksaveasfilename(filetypes = myfiletypes, title = "Сохранить как ....", defaultextension = myfiletypes) 
+    path = fd.asksaveasfilename(filetypes = my_filetypes, title = "Сохранить как ....", defaultextension = my_filetypes) 
     db = Connect(path)
-    ReadDB(db, 0)
+    Read_db(db, 0)
 
-def NewRec():
-    lblpath.config(text = path) 
-    enttitle.delete(0, END)       
-    Phones =[]        
-    Phones_var.set(Phones)
-    Mails =[]
-    boxmails.config(height=1)
-    Mails_var.set(Mails)
+def New_rec():
+    lbl_path.config(text = path) 
+    ent_title.delete(0, END)       
+    phones =[]        
+    phones_var.set(phones)
+    mails =[]
+    box_mails.config(height=1)
+    mails_var.set(mails)
     entday.delete(0, END)    
-    Addons = []    
-    boxAddons.config(height=1)
-    boxphone.config(height=1)    
-    Addons_var.set(Addons)
+    addons = []    
+    box_addons.config(height=1)
+    box_phone.config(height=1)    
+    addons_var.set(addons)
 
-def SearchRec():
+def Search_rec():
     rslt = []
     for item in records:
-        if entsearch.get() in item[0]:
+        if ent_search.get() in item[0]:
             rslt.append(item[0])
     if len(rslt) == 0:
         msgbox('Ничего не найдено')
@@ -109,88 +107,90 @@ def SearchRec():
         item = records[i]
         if item[0] == choice:
             index = i
-    ReadDB(db, index)
-        
+    Read_db(db, index)    
 
-    
-
-def NewAddon():
-    newvalue = simpledialog.askstring('Добавить', 'Добавьте информацию, для удобства используйте формат ТИП : Значение')
-    boxAddons.insert(END, newvalue)
-    if boxAddons.size() > len(Phones):
-        boxAddons.config(height=boxAddons.size())
-        boxphone.config(height=boxAddons.size())
+def New_addons():    
+    newvalue = enterbox('Добавьте информацию, для удобства используйте формат ТИП : Значение', 'Добавить', '' )
+    if newvalue == '':
+        return
+    box_addons.insert(END, newvalue)
+    if box_addons.size() > len(phones):
+        box_addons.config(height=box_addons.size())
+        box_phone.config(height=box_addons.size())
     else:
-        boxAddons.config(height=len(Phones))
-        boxphone.config(height=len(Phones))
+        box_addons.config(height=len(phones))
+        box_phone.config(height=len(phones))
     
-def NewPhone():
-    newvalue = simpledialog.askstring('Добавить', 'Добавьте номер телефона')
-    boxphone.insert(END, newvalue)
-    if boxphone.size() > len(Addons):
-        boxAddons.config(height=boxphone.size())
-        boxphone.config(height=boxphone.size())
+def New_phone():
+    newvalue = enterbox('Добавьте номер телефона', 'Добавить', '' )
+    if newvalue == '':
+        return    
+    box_phone.insert(END, newvalue)
+    if box_phone.size() > len(addons):
+        box_addons.config(height=box_phone.size())
+        box_phone.config(height=box_phone.size())
     else:
-        boxAddons.config(height=len(Addons))
-        boxphone.config(height=len(Addons))
+        box_addons.config(height=len(addons))
+        box_phone.config(height=len(addons))
 
-def NewMail():
-    newvalue = simpledialog.askstring('Добавить', 'Добавьте e-mail')
-    boxmails.insert(END, newvalue)
-    boxmails.config(height=boxmails.size())
+def New_email():
+    newvalue = enterbox('Добавьте e-mail', 'Добавить', '' )
+    if newvalue == '':
+        return     
+    box_mails.insert(END, newvalue)
+    box_mails.config(height=box_mails.size())
     
 
 window.title("Телефонный справочник")
 window.geometry('1024x600') 
-lblpath = Label(text = '', font = ("Arial", 10))
-lblpath.grid(row=0, column=0, pady=10, padx=10, columnspan=3)
-lbltitle = Label(text = 'Имя', font = ("Script MT Bold", 14))
-lbltitle.grid(row=1, column=0, pady=10, padx=10) 
-enttitle = Entry()
-enttitle.grid(row=1, column=1, pady=10, padx=10)
-lblphon = Label(text = 'Телефоны:', font = ("Script MT Bold", 14))
-lblphon.grid(row=2, column=0, pady=10, padx=10)
-Phones = []
-Phones_var = StringVar(value=Phones)
-boxphone = Listbox(listvariable=Phones_var, height=1)
-boxphone.grid(row=2, column=1, pady=10, padx=10)
-btnphone = Button(text = 'Добавить', command=NewPhone)
-btnphone.grid(row = 2, column=2, sticky='E')
-lblmail = Label(text = 'e-mail', font = ("Script MT Bold", 14))
-lblmail.grid(row=3, column=0, pady=10, padx=10) 
-lblday = Label(text = 'День рождения', font = ("Script MT Bold", 14))
-lblday.grid(row=4, column=0, pady=10, padx=10) 
-Mails = []
-Mails_var = StringVar(value=Mails)
-boxmails = Listbox(listvariable=Mails_var, height=1)
-boxmails.grid(row=3, column=1, pady=10, padx=10)
-btnmail = Button(text = 'Добавить', command=NewMail)
-btnmail.grid(row = 3, column=2, sticky='E')
+lbl_path = Label(text = '', font = ("Arial", 10))
+lbl_path.grid(row=0, column=0, pady=10, padx=10, columnspan=3)
+lbl_title = Label(text = 'Имя', font = ("Script MT Bold", 14))
+lbl_title.grid(row=1, column=0, pady=10, padx=10) 
+ent_title = Entry()
+ent_title.grid(row=1, column=1, pady=10, padx=10)
+lbl_phon = Label(text = 'Телефоны:', font = ("Script MT Bold", 14))
+lbl_phon.grid(row=2, column=0, pady=10, padx=10)
+phones = []
+phones_var = StringVar(value=phones)
+box_phone = Listbox(listvariable=phones_var, height=1)
+box_phone.grid(row=2, column=1, pady=10, padx=10)
+btn_phone = Button(text = 'Добавить', command=New_phone)
+btn_phone.grid(row = 2, column=2, sticky='E')
+lbl_mail = Label(text = 'e-mail', font = ("Script MT Bold", 14))
+lbl_mail.grid(row=3, column=0, pady=10, padx=10) 
+lbl_day = Label(text = 'День рождения', font = ("Script MT Bold", 14))
+lbl_day.grid(row=4, column=0, pady=10, padx=10) 
+mails = []
+mails_var = StringVar(value=mails)
+box_mails = Listbox(listvariable=mails_var, height=1)
+box_mails.grid(row=3, column=1, pady=10, padx=10)
+btn_mail = Button(text = 'Добавить', command=New_email)
+btn_mail.grid(row = 3, column=2, sticky='E')
 entday = Entry()
 entday.grid(row=4, column=1, pady=10, padx=10)
-lbladdon = Label(text = "Дополнительная информация", font = ("Script MT Bold", 14))
-lbladdon.grid(row=1, column=4, pady=10, padx=10, columnspan=2) 
-Addons = []
-Addons_var = StringVar(value = Addons)
-boxAddons = Listbox(listvariable = Addons_var, height=1, width=60)
-boxAddons.grid(row=2, column=4, columnspan=2, pady=10, padx=10)
-btnAddon = Button(text = 'Добавить', command=NewAddon)
-btnAddon.grid(row = 1, column=6, sticky='E')
-entsearch = Entry(width=40)
-entsearch.insert(string='Найти', index=0)
-entsearch.grid(row=0, column=5, pady=10, padx=10)
-btnsearch = Button(text = 'Поиск', command=SearchRec)
-btnsearch.grid(row = 0, column=6, sticky='E')
+lbl_addon = Label(text = "Дополнительная информация", font = ("Script MT Bold", 14))
+lbl_addon.grid(row=1, column=4, pady=10, padx=10, columnspan=2) 
+addons = []
+addons_var = StringVar(value = addons)
+box_addons = Listbox(listvariable = addons_var, height=1, width=60)
+box_addons.grid(row=2, column=4, columnspan=2, pady=10, padx=10)
+btn_addons = Button(text = 'Добавить', command=New_addons)
+btn_addons.grid(row = 1, column=6, sticky='E')
+ent_search = Entry(width=40)
+ent_search.insert(string='Найти', index=0)
+ent_search.grid(row=0, column=5, pady=10, padx=10)
+btn_search = Button(text = 'Поиск', command=Search_rec)
+btn_search.grid(row = 0, column=6, sticky='E')
 
 #меню
 menu = Menu(window)  
 new_item = Menu(menu)  
-new_item.add_command(label='Новый', command=NewDB)  
-new_item.add_command(label='Открыть', command=OpenDB)
+new_item.add_command(label='Новый', command=New_db)  
+new_item.add_command(label='Открыть', command=Open_db)
 menu.add_cascade(label='Файл', menu=new_item) 
 sec_item =Menu(menu)
-sec_item.add_command(label='Новая', command=NewRec)  
-sec_item.add_command(label='Найти', command=SearchRec)
+sec_item.add_command(label='Новая', command=New_rec)
 menu.add_cascade(label='Запись', menu=sec_item) 
 window.config(menu=menu)
 window.mainloop()
