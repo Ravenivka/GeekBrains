c67b2e54f9e79d687178030d9354a136a52e8a1a
Raven <raveniv@ya.ru>
2024-03-28 10:12:33 +0500

    запись карточки

--- Контакты/main.py
+++ Контакты/main.py
@@ -2,6 +2,7 @@ from sqlite3 import *
 from tkinter import *
 from tkinter import filedialog as fd
 from easygui import *
+import os
 
 #Декларация
 path = ''
@@ -33,8 +34,8 @@ def Read_db(Data : Connection, recindex):  #Opening DB
     mails =[word.strip() for word in item[2].split(';')]
     box_mails.config(height=len(mails))
     mails_var.set(mails)
-    entday.delete(0, END)
-    entday.insert(0,item[3])
+    ent_bday.delete(0, END)
+    ent_bday.insert(0,item[3])
     addons = [word.strip() for word in item[4].split(';')]
     if len(addons) > len(phones):
         box_addons.config(height=len(addons))
@@ -83,7 +84,7 @@ def New_rec():
     mails =[]
     box_mails.config(height=1)
     mails_var.set(mails)
-    entday.delete(0, END)    
+    ent_bday.delete(0, END)    
     addons = []    
     box_addons.config(height=1)
     box_phone.config(height=1)    
@@ -139,6 +140,44 @@ def New_email():
         return     
     box_mails.insert(END, newvalue)
     box_mails.config(height=box_mails.size())
+
+
+
+def Save_card(): 
+    global path 
+    global db   
+    if db == None: #автосоздание базы данных
+        counter = 1
+        filename = "Контакты(1).db"
+        while os.path.isfile(filename):
+            counter += 1
+            filename = "Контакты(" + str(counter) + ").db"
+        path = os.path.abspath(filename)
+        db = Connect(path)
+    rows = []
+    card_name = ent_title.get()
+    rows.append(card_name)
+    get_phones = ';'.join(box_phone.get(0, END))
+    rows.append(get_phones)
+    get_mails = ';'.join(box_mails.get(0, END))
+    rows.append(get_mails)
+    get_bd = ent_bday.get()
+    rows.append(get_bd)
+    get_addons = ';'.join(box_addons.get(0, END)) 
+    rows.append(get_addons)
+    curs = db.cursor() 
+    info = curs.execute('SELECT * FROM Контакты WHERE title=?', (card_name, )).fetchone()
+    if (info != None):
+        info = curs.execute('DELETE FROM Контакты WHERE title=?', (card_name, ))
+    curs.execute('INSERT into Контакты VALUES(?, ?, ?, ?, ?)', rows)
+    db.commit()  
+    Read_db(db, 0)
+   
+        
+
+    
+
+
     
 
 window.title("Телефонный справочник")
@@ -167,8 +206,8 @@ box_mails = Listbox(listvariable=mails_var, height=1)
 box_mails.grid(row=3, column=1, pady=10, padx=10)
 btn_mail = Button(text = 'Добавить', command=New_email)
 btn_mail.grid(row = 3, column=2, sticky='E')
-entday = Entry()
-entday.grid(row=4, column=1, pady=10, padx=10)
+ent_bday = Entry()
+ent_bday.grid(row=4, column=1, pady=10, padx=10)
 lbl_addon = Label(text = "Дополнительная информация", font = ("Script MT Bold", 14))
 lbl_addon.grid(row=1, column=4, pady=10, padx=10, columnspan=2) 
 addons = []
@@ -182,6 +221,8 @@ ent_search.insert(string='Найти', index=0)
 ent_search.grid(row=0, column=5, pady=10, padx=10)
 btn_search = Button(text = 'Поиск', command=Search_rec)
 btn_search.grid(row = 0, column=6, sticky='E')
+btn_save_card = Button(text='Сохранить изменения', command=Save_card)
+btn_save_card.grid(row=5, column=3, columnspan=2)
 
 #меню
 menu = Menu(window)  
